98a671e2faa6931eafd1e76b8dd0dfd9b2eb1676
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2024-07-01 10:05:27 +0700

    max 99 products

--- cart.php
+++ cart.php
@@ -86,7 +86,7 @@ if (isset($_GET['delete_all'])) {
                                 <td>
                                     <form action="" method="post">
                                         <input type="hidden" name="update_quantity_id" value="<?php echo $fetch_cart['id']; ?>">
-                                        <input type="number" name="update_quantity" min="1" value="<?php echo $fetch_cart['quantity']; ?>">
+                                        <input type="number" name="update_quantity" min="1" max="99" value="<?php echo $fetch_cart['quantity']; ?>">
                                         <input type="submit" value="Update" name="update_update_btn">
                                     </form>
                                 </td>
